3046da7b5368d7adf24110bd8eab7c22fa5659bb
maglars2 <maglars2@gmail.com>
2011-09-27 15:44:22 +0000

    Updated some server code, mostly test

---
+++ AndroidServer/src/util/ServerUtils.java
@@ -0,0 +1,73 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+public class ServerUtils {
+	
+	public static String getIp(){
+		URL myip;
+		String temp;
+		try {
+			myip = new URL("http://nollk.it/micro/ip.php");
+			BufferedReader in = new BufferedReader(new InputStreamReader(myip.openStream()));
+			temp = in.readLine();
+		}
+		catch(Exception e){
+			try{
+				temp = InetAddress.getLocalHost().getHostAddress();
+			}
+			catch(UnknownHostException e1){
+				return null;
+			}
+		}
+		return temp;
+	}
+	
+	public static String getLocalIp() {
+		String temp = "";
+		try{
+			InetAddress adr = getFirstNonLoopbackAddress(true, false);
+			temp = adr.getHostAddress();
+		}
+		catch(SocketException e){
+		}
+
+		return temp;
+	}
+
+	private static InetAddress getFirstNonLoopbackAddress(boolean preferIpv4,
+			boolean preferIPv6) throws SocketException {
+		Enumeration<?> en = NetworkInterface.getNetworkInterfaces();
+		while (en.hasMoreElements()) {
+			NetworkInterface i = (NetworkInterface) en.nextElement();
+			for (Enumeration<?> en2 = i.getInetAddresses(); en2.hasMoreElements();) {
+				InetAddress addr = (InetAddress) en2.nextElement();
+				if (!addr.isLoopbackAddress()) {
+					if (addr instanceof Inet4Address) {
+						if (preferIPv6) {
+							continue;
+						}
+						return addr;
+					}
+					if (addr instanceof Inet6Address) {
+						if (preferIpv4) {
+							continue;
+						}
+
+						return addr;
+					}
+				}
+			}
+		}
+		return null;
+	}
+}
