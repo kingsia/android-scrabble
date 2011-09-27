458b42e7d8b7181a9845d7970e9e2096231ee523
maglars2 <maglars2@gmail.com>
2011-09-27 17:08:48 +0000

    Updated the server and the controller with some codez and comments :D

--- AndroidServer/src/ServerController.java
+++ AndroidServer/src/ServerController.java
@@ -1,4 +1,56 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class ServerController {
+/**
+ * Server-thread that listens to the ServerSocket that the server provides.
+ * When a phone sends a request to the server it takes care of it and redirects
+ * to the correct model.
+ * 
+ * @author Magnus
+ */
+public class ServerController extends Thread implements Runnable{
 
+	private ServerSocket serverSocket = null;
+	private GameLogic gameLogic = null;	//	The logics of the game
+	private UserLogic userLogic = null;	//	The logics of the user
+	
+	/**
+	 * Creates a new ServerController from a Server and a ServerSocket.
+	 * 
+	 * @param server
+	 * @param sSocket
+	 */
+	public ServerController(Server server, ServerSocket sSocket){
+		
+		this.serverSocket = sSocket;
+		
+		gameLogic = new GameLogic();
+		userLogic = new UserLogic();
+		
+		gameLogic.addObserver(server);
+		userLogic.addObserver(server);
+	}
+
+	/**
+	 * Executes the thread
+	 */
+	public void run(){		
+		
+		while(true){
+			try{
+				Socket socket = serverSocket.accept();
+				ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+				Object o = in.readUnshared();
+				System.out.println(o.toString());
+			}
+			catch(IOException e){
+				e.printStackTrace();
+			}
+			catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
