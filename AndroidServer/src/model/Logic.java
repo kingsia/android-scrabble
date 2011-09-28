7b4e6cc8bb29493c56a229ea40e7b0bb1f4811fa
maglars2 <maglars2@gmail.com>
2011-09-28 13:43:51 +0000

    more stuff

--- AndroidServer/src/model/Logic.java
+++ AndroidServer/src/model/Logic.java
@@ -1,12 +1,19 @@
 package model;
 import java.util.Observable;
 
+/**
+ * Class that is extended by all logic-classes on the server.
+ * The Logic-class handles stuff like the database and makes sure
+ * that every logic-class is Observable.
+ * 
+ * @author Magnus
+ */
 public abstract class Logic extends Observable {
 
-	private Database db;
+	private Database db;	//	the database that all logic-classes work against
 	
 	protected Logic(){
-		db = new Database();
+		db = new Database();	//	Open db.
 	}
 
 	public Database getDatabase(){
