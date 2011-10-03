c06cbee10a5edb14ac06f45372bb99ff980aec7d
forssenm@gmail.com <forssenm@gmail.com@55f25e70-a808-2b91-0db6-e09dd0981900>
2011-10-03 16:02:15 +0000

    Started doing changes to the main.xml

--- trunk/AndroidScrabble/src/android/scrabble/MainMenuActivity.java
+++ trunk/AndroidScrabble/src/android/scrabble/MainMenuActivity.java
@@ -3,7 +3,7 @@ package android.scrabble;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class AndroidScrabbleActivity extends Activity {
+public class MainMenuActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
