747d8a77fcc7703badf8e3783bbe595a41a45e86
hansson.marika <hansson.marika@gmail.com>
2011-10-03 15:10:25 +0000

    changes to server networking

--- AndroidServer/src/Main.java
+++ AndroidServer/src/Main.java
@@ -1,13 +1,25 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import network.ServerInputThread;
+
 import controller.ServerController;
 
 
 public class Main {
-
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ServerController s = new ServerController(1337);
-		s.launch();
+		ServerSocket s = null;
+		try {
+			s = new ServerSocket();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		ServerController sc = new ServerController(s);
+		ServerInputThread st = new ServerInputThread(sc, s);
+		st.start();
 	}
 }
--- AndroidServer/src/network/ServerOutputThread.java
+++ AndroidServer/src/network/ServerOutputThread.java
@@ -1,5 +1,18 @@
 package network;
 
+import java.io.OutputStream;
+import java.net.ServerSocket;
+
 public class ServerOutputThread {
+	
+	private OutputStream out;
+	
+	public ServerOutputThread(ServerSocket s){
+		
+	}
+	
+	public void send(SendObject so){
+		
+	}
 
 }
