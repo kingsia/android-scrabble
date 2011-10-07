d0657d17dc0f7aeade5904eefb9c2de4bd67aa9b
forssenm@gmail.com <forssenm@gmail.com@55f25e70-a808-2b91-0db6-e09dd0981900>
2011-10-07 14:56:59 +0000

    Added SettingsViewActivity and some code to MainMenuActivity

--- trunk/AndroidScrabble/src/android/scrabble/MainMenuActivity.java
+++ trunk/AndroidScrabble/src/android/scrabble/MainMenuActivity.java
@@ -2,6 +2,7 @@ package android.scrabble;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainMenuActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,31 @@ public class MainMenuActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // The activity is about to become visible.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // The activity is about to be destroyed.
     }
 }
---
+++ trunk/AndroidScrabble/src/android/scrabble/SettingsViewActivity.java
@@ -0,0 +1,15 @@
+package android.scrabble;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsViewActivity extends Activity {
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_menu);
+    }
+
+}
